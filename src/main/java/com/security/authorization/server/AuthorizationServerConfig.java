3b8793d79566da464dc890de80009d8d857589f4
Tiago Pereira <tiago.pereira@hotmart.com>
2022-07-30 14:20:38 -0300

    Add Resource Server

--- src/main/java/com/security/authorization/server/AuthorizationServerConfig.java
+++ src/main/java/com/security/authorization/server/AuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package com.security;
+package com.security.authorization.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
