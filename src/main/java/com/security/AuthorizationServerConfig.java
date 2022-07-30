6f9f2cca389072b59dbfde94b66c13148b7a87d9
Tiago Pereira <tiago.pereira@hotmart.com>
2022-07-30 15:03:56 -0300

    remove resource server

--- src/main/java/com/security/AuthorizationServerConfig.java
+++ src/main/java/com/security/AuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package com.security.authorization.server;
+package com.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.security.authorization.server;
+package com.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
