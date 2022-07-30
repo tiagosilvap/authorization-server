91c774a0b476dce11f6e0c7df352d72d99a864a3
Tiago Pereira <tiago.pereira@hotmart.com>
2022-07-30 14:09:22 -0300

    Authorization Server

---
+++ src/main/java/com/security/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+package com.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    
+    /**
+     * Configurando clients em memória utilizando os fluxos de autenticaçào password e refresh_token
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("algafood-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6)
+                .and()
+                .withClient("checktoken")
+                .secret(passwordEncoder.encode("check123"));
+    }
+    
+    
+    /**
+     * Autorizar resource owners sem a necessidade de passar o client id e secret id nas chamadas
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//		security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("permitAll()");
+    }
+    
+    /**
+     * authenticationManager - Necessário para solicitações Password Flow
+     * userDetailsService - Necessário para solitações Refresh Token
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+    
+}
